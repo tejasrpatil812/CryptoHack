9506acedde81a987b57631c38449a72ade752e6d
Tejas Patil <tejasrpatil@hotmail.com>
2022-09-07 11:50:18 -0700

    Initial Commit

---
+++ great_snakes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 2, 92, 79]
+
+print("Here is your flag:")
+print("".join(chr(o ^ 0x32) for o in ords))
